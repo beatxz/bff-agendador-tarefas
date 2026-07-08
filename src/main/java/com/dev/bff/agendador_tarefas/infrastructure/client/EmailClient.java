0e12c87da54bfd51195454ce68e8487a53891bd3
bea <164930551+beatxz@users.noreply.github.com>
2026-07-08 11:23:15 -0300

    Adicionado Client email

---
+++ src/main/java/com/dev/bff/agendador_tarefas/infrastructure/client/EmailClient.java
@@ -0,0 +1,12 @@
+package com.dev.bff.agendador_tarefas.infrastructure.client;
+
+import com.dev.bff.agendador_tarefas.business.dto.out.TarefasDTOResponse;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.*;
+
+@FeignClient(name = "notificacao", url = "${notificacao.url}")
+public interface EmailClient {
+
+    void enviarEmail(@RequestBody TarefasDTOResponse dto);
+}
+
